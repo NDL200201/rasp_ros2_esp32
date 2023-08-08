b6acb1a6473ed595d76367b564a0d7c81a3fe1b5
NDL200201 <nguyenduonglinh20@gmail.com>
2023-08-08 10:36:06 +0700

    first commit

---
+++ encoder_driver.h
@@ -0,0 +1,18 @@
+   
+#ifdef ARDUINO_ENC_COUNTER
+
+
+  #define LEFT_ENC_PIN_A 19   // pin 16
+  #define LEFT_ENC_PIN_B 21   // pin 17
+
+  #define RIGHT_ENC_PIN_A 32  //pin A4
+  #define RIGHT_ENC_PIN_B 33   //pin A5
+  #define ENC_COUNT_REV 200;
+#endif
+   
+long readEncoder(int i);
+void resetEncoder(int i);
+void resetEncoders();
+
+void L_ENA();
+void R_ENA();
